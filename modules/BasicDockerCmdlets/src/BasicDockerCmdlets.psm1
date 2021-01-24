ec859c0ce1298dfbb648fae757275c987eebe5b2
Victor Rodilla <victorrodilla@outlook.com>
2021-01-24 14:30:03 +0100

    Create BasicDockerCmdlets.psm1

---
+++ modules/BasicDockerCmdlets/src/BasicDockerCmdlets.psm1
@@ -0,0 +1,128 @@
+# Script CBH-1
+<# .SYNOPSIS
+     Docker first steps 
+.DESCRIPTION
+     With this functions you be able to start, stop and restart docker containers easily from powershell
+.NOTES
+     Author     : Victor ROdilla - victorrodilla@outlook.com
+.LINK
+     https://github.com/victorrodilla/
+#>
+  
+function Get-DockerExist {
+    param (
+        [Parameter(Mandatory)]
+        [string]$Name
+    )
+
+    if (docker ps -a --format "table {{.Names}}" | Select-String $Name) {
+        return $true
+    } else {
+        return $false
+    }
+
+}
+Export-ModuleMember -Function Get-DockerExist
+
+function Get-DockerStatus {
+
+    param (
+        [Parameter(Mandatory)]
+        [string]$Name
+    )
+    
+    $output = New-Object PsObject -Property @{Name=$Name}
+    
+    if (DockerExist -Name $Name) {
+
+        #Prepare JSON from docker inspect
+        $json=docker inspect $Name | ConvertFrom-Json         #Only show docker status
+        $status=$json.state.status
+        
+        #Create Property Status
+        Add-Member -InputObject $output -MemberType NoteProperty -Name Status -Value $status
+
+        return $output
+
+    } else {
+
+        #Create property Status with No Exist
+        Add-Member -InputObject $output -MemberType NoteProperty -Name Status -Value "non-existent"
+        return $output
+
+    }
+
+}
+Export-ModuleMember -Function Get-DockerStatus
+
+function Start-Docker {
+
+    param (
+        [Parameter(Mandatory)]
+        [string]$Name
+    )
+
+    $status=Get-DockerStatus -Name $Name | Select-Object -Property Status -ExpandProperty Status
+
+    if (!$status.Contains("non-existent") -and !$status.Contains("running")) {
+
+         docker start $Name | Out-Null
+         $response=docker ps -a --no-trunc --filter name=^/$Name$
+         Write-Output $response  
+    
+    } elseif ($status.Contains("non-existent")) {
+
+        $response="CONTAINER with name $Name is not exists"
+        Write-Output $response
+
+    }
+
+}
+Export-ModuleMember -Function Start-Docker
+
+function Stop-Docker {
+
+    param (
+        [Parameter(Mandatory)]
+        [string]$Name
+    )
+    
+    $status=Get-DockerStatus -Name $Name | Select-Object -Property Status -ExpandProperty Status
+
+    if ($status -match "running") {
+
+         docker stop $Name | Out-Null
+         $response=docker ps -a --no-trunc --filter name=^/$Name$
+         Write-Output $response 
+
+    } elseif ($status.Contains("non-existent")) {
+
+        $response="CONTAINER with name $Name is not exists"
+        Write-Output $response
+
+    }
+
+}
+Export-ModuleMember -Function Stop-Docker
+
+function Restart-Docker {
+
+    param (
+        [Parameter(Mandatory)]
+        [string]$Name
+    )
+    if (Check-DockerExist -Name $Name) {
+
+        docker restart $Name | Out-Null
+        $response=docker ps -a --no-trunc --filter name=^/$Name$
+        Write-Output $response
+    } elseif ($status.Contains("non-existent")) {
+
+        $response="CONTAINER with name $Name is not exists"
+        Write-Output $response
+    
+    }
+         
+
+}
+Export-ModuleMember -Function Restart-Docker
